9aa92e5a2cec1779de8cd777d8754eb2c822317a
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-02-26 10:52:22 +0200

    Currency.java model for all banks was updated

--- src/main/java/com/chimyrys/currencyservice/model/Currency.java
+++ src/main/java/com/chimyrys/currencyservice/model/Currency.java
@@ -1,10 +1,18 @@
 package com.chimyrys.currencyservice.model;
 
+import org.springframework.beans.propertyeditors.CurrencyEditor;
+
+/**
+ * Class that describes model of currency.
+ * Consists of values of RUB/USD/EUR/UAH
+ * and its three-digits id due to ISO 4217
+ */
 public enum Currency {
-    RUB("RUB", 1),
-    USD("USD", 2),
-    EUR("EUR", 3),
-    UAH("UAH", 4);
+    RUB("RUB", 643),
+    USD("USD", 840),
+    EUR("EUR", 978),
+    UAH("UAH", 980),
+    DEFAULT_INSTANCE("",-1);
     private final String value;
     private final int id;
     Currency(String value, int id) {
@@ -19,4 +27,14 @@ public enum Currency {
     public int getId() {
         return id;
     }
+
+    public Currency getValueFromId(int id) {
+        for (Currency c : values()) {
+            if (c.id == id) {
+                return c;
+            }
+        }
+        return null;
+    }
+
 }
