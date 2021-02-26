e915e73ca66fbfd51f6f473e6e3da04d636d11c5
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-02-26 10:59:31 +0200

    MonoBankExchangeRateResponse.java was updated

---
+++ src/main/java/com/chimyrys/currencyservice/model/MonoBankExchangeRateResponse.java
@@ -0,0 +1,23 @@
+package com.chimyrys.currencyservice.model;
+
+import java.util.List;
+
+/**
+ * Class that have List of MonobankExchangeRate Objects
+ * got from Monobank API (JSON file)
+ */
+public class MonoBankExchangeRateResponse {
+    private List<MonobankExchangeRate> monobankExchangeRateList;
+
+    public MonoBankExchangeRateResponse(List<MonobankExchangeRate> monobankExchangeRateList) {
+        this.monobankExchangeRateList = monobankExchangeRateList;
+    }
+
+    public List<MonobankExchangeRate> getMonobankExchangeRateList() {
+        return monobankExchangeRateList;
+    }
+
+    public void setMonobankExchangeRateList(List<MonobankExchangeRate> monobankExchangeRateList) {
+        this.monobankExchangeRateList = monobankExchangeRateList;
+    }
+}
