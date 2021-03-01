c76dc6bf08b3a89cf05e8d26f04b241887dfe471
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-03-01 21:33:10 +0200

    getResponseBodyFromBank() method was added

--- src/main/java/com/chimyrys/currencyservice/service/impl/MonobankCurrencyService.java
+++ src/main/java/com/chimyrys/currencyservice/service/impl/MonobankCurrencyService.java
@@ -13,7 +13,9 @@ import org.apache.http.util.EntityUtils;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.io.IOException;geimport java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -35,6 +37,50 @@ public class MonobankCurrencyService implements CurrencyService {
 
     @Override
     public ExchangeRate getCurrency(RateDate rateDate, Currency currencyFrom, Currency currencyTo) {
+        String response = getResponseBodyFromBank();
+        MonoBankExchangeRateResponse monoBankExchangeRateResponse = conversionService.convert(response, MonoBankExchangeRateResponse.class);
+        if (monoBankExchangeRateResponse == null) {
+            return null;
+        }
+            try {
+                return Stream.of(monoBankExchangeRateResponse.getMonobankExchangeRateList())
+                        .map(monobankExchangeRates -> conversionService.convert(monobankExchangeRates, ExchangeRate.class))
+                        .filter(Objects::nonNull)
+                        .filter(exchangeRate -> exchangeRate.getDateTime().equals(rateDate))
+                        .filter(exchangeRate -> exchangeRate.getCurrencyTo().equals(currencyTo))
+                        .filter(exchangeRate -> exchangeRate.getCurrencyFrom().equals(currencyFrom))
+                        .iterator().next();
+            } catch (NoSuchElementException e) {
+                return null;
+            }
+    }
+
+    @Override
+    public ExchangeRate getBestCurrencyForMonth(Currency currencyFrom, Currency currencyTo) {
+        String response = getResponseBodyFromBank();
+        MonoBankExchangeRateResponse monoBankExchangeRateResponse = conversionService.convert(response, MonoBankExchangeRateResponse.class);
+        if (monoBankExchangeRateResponse == null) {
+            return null;
+        }
+        RateDate currentRateDate = RateDate.setDate(Instant.now().getEpochSecond());
+        System.out.println(currentRateDate);
+       /* try {
+            return Stream.of(monoBankExchangeRateResponse.getMonobankExchangeRateList())
+                    .map(monobankExchangeRates -> conversionService.convert(monobankExchangeRates, ExchangeRate.class))
+                    .filter(Objects::nonNull)
+                    .filter(exchangeRate -> exchangeRate.ge)
+        } catch (NoSuchElementException e) {
+            return null;
+        }*/
+        return null;
+    }
+
+    @Override
+    public ExchangeRate getBestCurrencyForWeek(Currency currencyFrom, Currency currencyTo) {
+        return null;
+    }
+
+    private String getResponseBodyFromBank() {
         String http = "https://api.monobank.ua/bank/currency";
         HttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(http);
@@ -46,24 +92,7 @@ public class MonobankCurrencyService implements CurrencyService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        MonoBankExchangeRateResponse monoBankExchangeRateResponse = conversionService.convert(json, MonoBankExchangeRateResponse.class);
-        return Stream.of(monoBankExchangeRateResponse.getMonobankExchangeRateList())
-                .map(monobankExchangeRates -> conversionService.convert(monobankExchangeRates, ExchangeRate.class))
-                .filter(Objects::nonNull)
-                .filter(exchangeRate -> exchangeRate.getDateTime().equals(rateDate))
-                .filter(exchangeRate -> exchangeRate.getCurrencyTo().equals(currencyTo))
-                .filter(exchangeRate -> exchangeRate.getCurrencyFrom().equals(currencyFrom))
-                .iterator().next();
-    }
-
-    @Override
-    public ExchangeRate getBestCurrency(RateDate rateDate, Currency currencyFrom, Currency currencyTo) {
-        return null;
-    }
-
-    @Override
-    public ExchangeRate getBestCurrencyForWeek(Currency currencyFrom, Currency currencyTo) {
-        return null;
+        return json;
     }
 
     @Override
