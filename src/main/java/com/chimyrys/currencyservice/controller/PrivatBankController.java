3aa977caeb1c6e5a7af3b31aa6d937aa02a4ef4e
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-03-24 13:53:33 +0200

    Unused loggers were deleted

--- src/main/java/com/chimyrys/currencyservice/controller/PrivatBankController.java
+++ src/main/java/com/chimyrys/currencyservice/controller/PrivatBankController.java
@@ -2,14 +2,12 @@ package com.chimyrys.currencyservice.controller;
 
 import com.chimyrys.currencyservice.service.api.CurrencyService;
 import com.chimyrys.currencyservice.service.api.SaveInfoService;
-import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/privatbank")
 public class PrivatBankController extends Controller {
-    private final static Logger logger = Logger.getLogger(PrivatBankController.class);
 
     public PrivatBankController(CurrencyService privatbankCurrencyService, SaveInfoService saveInfoService) {
         super(saveInfoService);
